426af46ba7bc170f4b9d2604a8728cbebf1e3c06
Yosuke Tanigawa <ytanigaw@stanford.edu>
2020-08-08 14:48:14 -0700

    add GWAS scripts for the imputed dataset

---
+++ notebook/snpnet-PRScs/2_gwas_imp_parallel.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+set -beEuo pipefail
+
+ml load plink2/20200725
+
+# We installed PLINK2 software as a software module in our HPC system.
+# This `ml load plink2/20200725` updates the PATHs so that we can execute plink2 software.
+
+batch_idx=${SLURM_ARRAY_TASK_ID:=1}
+if [ $# -gt 0 ] ; then batch_idx=$1 ; fi
+GBE_ID=$2
+n_batch=100
+
+# pfile="/oak/stanford/groups/mrivas/ukbb24983/array_imp_combined/pgen_v2/ukb24983_cal_hla_cnv_imp"
+pfile="/scratch/groups/mrivas/ukbb24983/array_imp_combined/pgen_v2/ukb24983_cal_hla_cnv_imp"
+
+pvar_n_vars=$(zstdcat ${pfile}.pvar.zst | egrep -v '^#' | wc -l)
+batch_size=$(perl -e "print(  int((${pvar_n_vars}-1)/${n_batch}) + 1  )")
+idx_s=$(perl -e "print(  ${batch_size} *  (${batch_idx} - 1) + 1  )")
+idx_e=$(perl -e "print(  ${batch_size} *   ${batch_idx} )")
+
+out_d="/oak/stanford/groups/mrivas/projects/biobank-methods-dev/snpnet-GWAS/sumstats_imp_train_val"
+
+if [ ! -f ${out_d} ] ; then mkdir -p ${out_d} ; fi
+
+cat /oak/stanford/groups/mrivas/projects/biobank-methods-dev/snpnet-elastic-net/phenotype.phe \
+| awk '($15 == "train" || $15 == "val"){print $1, $2}' \
+| plink2 \
+--memory 6000 --threads 2 \
+--keep /dev/stdin \
+--maf 0.001 --geno 0.1 \
+--extract <(zstdcat ${pfile}.pvar.zst | egrep -v '^#' | awk -v idx_s=${idx_s} -v idx_e=${idx_e} '(idx_s <= NR && NR <= idx_e){print $3}') \
+--pfile ${pfile} vzs \
+--pheno /oak/stanford/groups/mrivas/projects/biobank-methods-dev/snpnet-elastic-net/${GBE_ID}.phe \
+--covar /oak/stanford/groups/mrivas/projects/biobank-methods-dev/snpnet-elastic-net/phenotype.phe \
+--covar-name age sex PC1-PC10 \
+--glm firth-fallback hide-covar omit-ref no-x-sex \
+--out ${out_d}/${GBE_ID}.batch${batch_idx}
