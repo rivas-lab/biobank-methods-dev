1ac8219c1b7c88ed588112b8b62832a2bd7ae03a
Yosuke Tanigawa <ytanigaw@stanford.edu>
2021-04-23 23:50:39 -0700

    add notebook to browse GREAT results

--- notebook/20210422_SRRR/Asthma/parameters.sh
+++ notebook/20210422_SRRR/Asthma/parameters.sh
@@ -1,6 +1,15 @@
+# the RData from SRRR
+RData_d='/oak/stanford/groups/mrivas/users/mrivas/repos/multiresponse-ukbb/results_asthma_rank_5_unweighted'
+RData_path='results_rank_5/output_lambda_67.RData'
+
+results_sub_d='results_rank_5'
+rdata_base='output_lambda_67'
+
 # we will save our data here
 
 data_d='/oak/stanford/groups/mrivas/projects/biobank-methods-dev/20210422_SRRR/Asthma'
+ref_d='/oak/stanford/groups/mrivas/projects/biobank-methods-dev/20210422_SRRR/ref'
+
 
 # on the Bejerano lab server, we put the GREAT input/output here
 great_d='/cluster/u/ytanigaw/tmp/20210422_great'
@@ -12,10 +21,7 @@ ontology='MGIPhenoSingleKO'
 #  variant annotation file
 var_annot_f='/oak/stanford/groups/mrivas/ukbb24983/array-combined/annotation/annotation_20201012/ukb24983_cal_hla_cnv.annot_compact_20201023.tsv.gz'
 
-# the RData from SRRR
-RData_d='/oak/stanford/groups/mrivas/users/mrivas/repos/multiresponse-ukbb/results_asthma_rank_5_unweighted'
-
-RData_path='results_rank_5/output_lambda_67.RData'
+onto_terms='ontoTerms.canon'
 
 # output
 variant_weights_f='variant_weights.tsv'
